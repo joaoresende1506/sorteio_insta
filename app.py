f763331aa0f9f91d7314585d900c3caf79f1cfa8
joaoresende1506 <joaoresende1506@gmail.com>
2024-05-26 16:14:35 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import random
 import matplotlib.pyplot as plt
 
+st.set_page_config(page_title='Sorteio Dra Mylena e Mare Acessórios')
+
 # Título e subtítulo do aplicativo
 st.title('Sorteio Dra. Mylena Braga e Mare Acessórios')
 st.subheader('Boa sorte !')
@@ -10,7 +12,7 @@ st.subheader('Boa sorte !')
 # Divisor e informações sobre o sorteio
 st.divider()
 st.write('O sorteio irá funcionar da seguinte maneira:')
-st.write(' - Todos os comentários feitos até o prazo definido pelo sorteio foram extraídos para uma planilha.')
+st.write(' - Todos os comentários realizados até o prazo definido pelo sorteio foram extraídos para uma planilha.')
 st.write(' - Após o clique no botão será sorteado um número e iremos buscar dentro da planilha qual registro é esse comentário.')
 st.caption('Obs: Os comentários foram ordenados de forma crescente pela data de registro.')
 st.caption('Obs²: A hora está em formato e tempo civil americano.')
@@ -21,8 +23,32 @@ comentarios = pd.read_excel('comentarios.xlsx')
 # Ordena os comentários pela hora
 comentarios.sort_values(by='Hora', inplace=True)
 
+# Variável de sessão para controlar a confirmação
+if 'confirma' not in st.session_state:
+    st.session_state.confirma = False
+
+
+# Função para exibir o modal de confirmação
+@st.experimental_dialog('Confirme sua ação')
+def show_confirmation_modal():
+    st.write("Tem certeza que deseja realizar o sorteio?")
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button('Sim', key='confirm_sorteio'):
+            st.session_state.confirma = True
+            st.experimental_rerun()
+    with col2:
+        if st.button('Não', key='cancel_sorteio'):
+            st.session_state.confirma = False
+            st.experimental_rerun()
+
 # Botão para realizar o sorteio
 if st.button('Sortear comentário'):
+    show_confirmation_modal()
+
+# Realiza o sorteio se confirmado
+if st.session_state.confirma:
+    st.balloons()
     # Seleciona um índice aleatório
     sorteado_idx = random.randint(0, len(comentarios) - 1)
     
@@ -40,8 +66,10 @@ if st.button('Sortear comentário'):
     )
     st.write(f'Com o comentário: {comentario_sorteado[1]}')
     st.write(f'Realizado no dia: {comentario_sorteado[2]}')
+    st.session_state.confirma = False
 
 if st.button('Limpar sorteio'):
+    st.session_state.confirma = False
     st.experimental_rerun()
 
 # Exibe todos os comentários
